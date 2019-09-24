1c7c60da8c9d96497c1f34d2781cb27f650fa5e4
JonAnderAsua <49275812+JonAnderAsua@users.noreply.github.com>
2019-09-24 15:17:02 +0200

    -

---
+++ Pelikula.java
@@ -0,0 +1,14 @@
+package packlab1;
+
+public class Pelikula {
+	private String izenBurua;
+	private int dirua;
+	
+	public Pelikula(String pIzenBurua) {
+		this.izenBurua=pIzenBurua;
+	}
+	
+	public void setDirua(int pDirua) {
+		this.dirua=pDirua;
+	}
+}
