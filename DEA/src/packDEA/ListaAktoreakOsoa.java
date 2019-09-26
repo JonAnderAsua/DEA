99d6726c2c0a5f3b3a277faef57c9ea0427ec5b9
jonander <jonander@jonAnder>
2019-09-26 19:39:28 +0200

    d

--- DEA/src/packDEA/ListaAktoreakOsoa.java
+++ DEA/src/packDEA/ListaAktoreakOsoa.java
@@ -4,8 +4,6 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-//import java.io.FileReader;
 import java.io.IOException;
 
 public class ListaAktoreakOsoa {
