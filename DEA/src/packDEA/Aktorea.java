b5bea4d7ca3fffac68616b33b92dd09815834812
jonander <jonander@jonAnder>
2019-09-26 19:48:13 +0200

    efwefw

--- DEA/src/packDEA/Aktorea.java
+++ DEA/src/packDEA/Aktorea.java
@@ -4,6 +4,7 @@ public class Aktorea {
 	private String izenAbizena;
 	private listaFilmak lista;
 	
+	
 	public Aktorea(String pIzenAbizena){
 		this.izenAbizena=pIzenAbizena;
 		this.lista=this.pelikulenLista();
