d2af1b663f7f7fd04223c5cf50b55db69354fccd
jonander <jonander@jonAnder>
2019-10-01 18:49:04 +0200

    ListaFilmak

--- DEA/src/packDEA/listaFilmak.java
+++ DEA/src/packDEA/listaFilmak.java
@@ -7,11 +7,16 @@ import java.util.Iterator;
 public class listaFilmak {
 	private ArrayList<Pelikula> lista;
 	
+	
 	public listaFilmak(){
 		this.lista=new ArrayList<Pelikula>();
 	}
 	
 	public boolean badago(Pelikula pPelikula) {
 		return this.lista.contains(pPelikula);
+	}
+	
+	public void pelikulaGehitu(Pelikula pPelikula) {
+		
 	}
 }
