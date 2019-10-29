e901f40893b812a3b8ef8585fe2506fb654c4b09
jonander <jonander@jonAnder>
2019-10-29 17:30:01 +0100

    Zuzenketak

--- Dea/src/packBigarrenPraktika/OrderedDoubleLinkedList.java
+++ Dea/src/packBigarrenPraktika/OrderedDoubleLinkedList.java
@@ -13,32 +13,46 @@ public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements O
 			berria.prev=berria.next=berria;
 		}
 		else {
-			lag=first;
-			while(konp.compareTo(lag.data)<0&&lag.next!=first) {
-				lag=lag.next;
+			if(!this.contains(elem)) {
+				lag=first;
+				while(konp.compareTo(lag.data)<0&&lag.next!=first) {
+					lag=lag.next;
+				}
+				berria.prev=lag;
+				berria.next=lag.next;
+				lag.next.prev=berria;
+				lag.next=berria;
 			}
-			berria.prev=lag;
-			berria.next=lag.next;
-			lag.next.prev=berria;
-			lag.next=berria;
 		}
-	}
+	}	
 	
 	//Kostua = n, kasurik txarrenean lista osoa errekorritu behar duzulako 
 
 	public void merge(DoubleLinkedList<T> zerrenda){
 		// KODEA OSATU ETA KOSTUA KALKULATU
 		Node<T>berria;
-		Comparable<T>lehenengoa=(Comparable<T>)first;
-		if(lehenengoa.compareTo(zerrenda.first())<1){ //Sartutako listaren datuak dauden datuak baino handiago badira
-			for(int i=0;i<=zerrenda.size();i++) {
+		Comparable<T>azkena=(Comparable<T>)last();
+		if(isEmpty()) {
+			for(int i=0;i<=zerrenda.size();i++){
 				berria=zerrenda.posizioanLortu(i);
-				gehituAmaieran(berria);
-			}
+				this.gehituAmaieran(berria);
+			}	
 		}
-		for(int i=0;i<=zerrenda.size();i++) {
-			berria=zerrenda.posizioanLortu(i);
-			add(berria.data);
+		else {
+			if(!zerrenda.isEmpty()) {
+				if(azkena.compareTo(zerrenda.first())<1){ //Sartutako listaren datuak dauden datuak baino handiago badira
+					for(int i=0;i<=zerrenda.size();i++) {
+						berria=zerrenda.posizioanLortu(i);
+						gehituAmaieran(berria);
+					}
+				}
+				else {
+					for(int i=0;i<=zerrenda.size();i++) {
+						berria=zerrenda.posizioanLortu(i);
+						add(berria.data);
+					}
+				}
+			}
 		}
 	}
 }
