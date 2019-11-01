23a64aa73bdf70980b4afff2560dcff2fe55c2b6
jonander <jonander@jonAnder>
2019-11-01 17:24:30 +0100

    ListaFilmak nodoekin

--- DEA/src/packDEA/listaFilmak.java
+++ DEA/src/packDEA/listaFilmak.java
@@ -5,25 +5,47 @@ import java.util.Iterator;
 
 
 public class listaFilmak {
-	private ArrayList<Pelikula> lista;
+	private Node<Pelikula> first;
 	
 	
 	public listaFilmak(){
-		this.lista=new ArrayList<Pelikula>();
+		this.first=null;
 	}
 	
 	
 	public boolean badago(Pelikula pPelikula) {
 		//Aurre:
 		//Post: Zerrendan Pelikula dagoen ala ez adierazten du, badago true bueltatzen du, bestela false
-		return this.lista.contains(pPelikula);
+		boolean emaitza=false;
+		Node<Pelikula> lag;
+		if(!isEmpty()) {
+			emaitza=first.data.equals(pPelikula);
+			lag=first.next;
+			while(lag!=first && !emaitza) {
+				emaitza=lag.data.equals(pPelikula);
+				lag=lag.next;
+			}
+		}
+		return emaitza;
 	}
 	
 	public void pelikulaGehitu(Pelikula pPelikula) {
 		//Aurre:
-		//Post: Pelikula ez badago zerrendan gehitzen du
-		if(!this.lista.contains(pPelikula)) {
-			lista.add(pPelikula);
+		//Post: Pelikula ez badago zerrendan AMAIERAN gehitzen du
+		Node<Pelikula> berria = new Node(pPelikula);
+		if(isEmpty()) {
+			first=berria;
+			first.prev=first.next=berria;
 		}
+		else {
+			berria.prev=first.prev;
+			berria.next=first;
+			first.prev.next=berria;
+			first.prev=berria;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return(first==null);
 	}
 }
